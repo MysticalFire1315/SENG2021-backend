874015184ad7b1b772f7349d5137e7f940b57fbd
MysticalFire1315 <joelchong03@gmail.com>
2023-03-03 22:39:52 +1100

    Removed comments

--- src/models/invoice.model.ts
+++ src/models/invoice.model.ts
@@ -28,34 +28,3 @@ export class InvoiceModel {
     return './invoice1.xml';
   }
 }
-
-type InvoiceLine = {
-  ID: number;                                         // BR-21
-  ItemName: number;                                   // BR-25
-  ItemNetPrice: number;                               // BR-26 this is for just 1 single invoice line
-  InvoiceQuantity: number;                            // BR-22
-  MeasurementUnitCode: string;                        // BR-23 simply the units for an item; i.e 5kg of pork, 10Oz of pencils
-  CurrentNetAmount: number;                           // BR-12
-  // etc
-};
-
-// const exampleObj = {
-//   SpecificationId: '20asdlfjk2423',
-//   InvoiceNumber: 5465231,
-//   IssueDate: 'YYYY-MM-DD',
-//   // etc
-//   SellerPostalAddress: {
-//     StreetName: 'Somewhere',
-//     CityName: 'Sydney',
-//     PostalZone: '2154',
-//     Country: { IdentificationCode: '34o8asdflk' },
-//   },
-// };
-
-// const jsonStr = JSON.stringify(exampleObj, null, 2);
-
-// console.log(jsonStr);
-
-// const jsonObj = JSON.parse(jsonStr);
-
-// console.log(jsonObj);
