c3f33a75dde05e3c9b080020e7f60226655602b9
MysticalFire1315 <joelchong03@gmail.com>
2023-03-09 15:30:54 +1100

    Added initial templates for all routes

--- src/creation/creation.controller.ts
+++ src/creation/creation.controller.ts
@@ -1,4 +1,31 @@
-import { Controller } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Param,
+  Post,
+  UploadedFile,
+  UseInterceptors,
+} from '@nestjs/common';
+import { FileInterceptor } from '@nestjs/platform-express';
+import { CreationService } from './creation.service';
 
 @Controller('creation')
-export class CreationController {}
+export class CreationController {
+  constructor(private creationService: CreationService) {}
+
+  // Routes here
+
+  @Post('upload')
+  @UseInterceptors(FileInterceptor('file'))
+  async uploadFile(@UploadedFile() file: Express.Multer.File) {
+    const output = await this.creationService.invoiceUpload(file);
+
+    return output;
+  }
+
+  @Get('download')
+  async downloadFile(@Param('token') token: string) {
+    const output = await this.creationService.invoiceDownload(token);
+    return output;
+  }
+}
--- src/creation/creation.service.ts
+++ src/creation/creation.service.ts
@@ -1,4 +1,41 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, StreamableFile } from '@nestjs/common';
+import { Invoice } from './model/invoice.model';
 
 @Injectable()
-export class CreationService {}
+export class CreationService {
+  private invoiceList: {
+    object: Invoice;
+    token: string;
+    inUse: boolean;
+  }[] = [];
+
+  async invoiceUpload(
+    file: Express.Multer.File,
+  ): Promise<{ timeEstimate: number; token: string }> {
+    const invoice = {
+      object: new Invoice(),
+      token: 'abc', // generateToken(), // Needs implementation
+      inUse: true,
+    };
+
+    invoice.object.parse(file.buffer.toString()).then(() => {
+      invoice.inUse = false;
+    });
+
+    this.invoiceList.push(invoice);
+
+    // TODO: Determine time estimate
+
+    return { timeEstimate: 1, token: invoice.token };
+  }
+
+  async invoiceDownload(token: string): Promise<StreamableFile> {
+    const invoice = this.invoiceList.find((invoice) => invoice.token === token);
+
+    const returnFile = new StreamableFile(
+      Buffer.from(await invoice.object.createUBL()),
+    );
+
+    return returnFile;
+  }
+}
