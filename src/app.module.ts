60245b6ab5512010023892b1eb59fcd8a3732727
MysticalFire1315 <joelchong03@gmail.com>
2023-03-09 12:39:24 +1100

    Added initial template for creation API

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { CreationModule } from './creation/creation.module';
 
 @Module({
-  imports: [],
+  imports: [CreationModule],
   controllers: [AppController],
   providers: [AppService],
 })
