c4fc26255fc7ea56a2473a1c2bcaec7880a7924b
derpyhead999 <70264124+derpyhead999@users.noreply.github.com>
2023-03-10 17:16:44 +1100

    Developed parse tests

--- createUBL.test.ts
+++ createUBL.test.ts
@@ -1,63 +1,60 @@
 import { InvoiceModel } from "./invoice.model";
 import { InvoiceSpecification } from "./invoice.json";
 const fs = require('fs');
-
-function deleteFile(fileName: String): void {
-  fs.unlink(fileName, (err: Error) => {
-    if (err) throw err;
-    console.log('File is successfully deleted');
-  });
-}
-afterEach(() => {
-  deleteFile('invoice1.xml');
-});
-// Concern about telephone numbers treated as numbers, but start with 0
+const { convert } = require('xmlbuilder2');
 
 describe("test InvoiceModel createUBL", () => {
   test("Mandatory Field Input", () => {
     const newInvoice: InvoiceModel = new InvoiceModel();
-    newInvoice.parse(""); // Need to parse some kinda file from user input
+    newInvoice.parse(""); // Need to parse some kinda object string from user input
 
-    newInvoice.createUBL(); //Hopefully returns invoice1.xml in same directory
-    const actual = fs.readFileSync('./invoice1.xml');
+    const actual = newInvoice.createUBL();
     const expected = fs.readFileSync('./testMandatoryInput/InvoiceLine1M.xml');
-    expect(expected).toStrictEqual(actual);
+    const actualObj = convert(actual, { format: "object" });
+    const expectedObj = convert(expected, { format: "object" });
+    expect(expectedObj).toStrictEqual(actualObj);
   });
   test("Mandatory Fields >2 Decimal Place Condition", () => {
     const newInvoice: InvoiceModel = new InvoiceModel();
     newInvoice.parse("");
 
-    newInvoice.createUBL();
-    const actual = fs.readFileSync('./invoice1.xml');
+    const actual =  newInvoice.createUBL();
     const expected = fs.readFileSync('./testMandatoryInput/DecimalPointOverM.xml');
-    expect(expected).toStrictEqual(actual);
+    const actualObj = convert(actual, { format: "object" });
+    const expectedObj = convert(expected, { format: "object" });
+    expect(expectedObj).toStrictEqual(actualObj);
   });
   test("Optional Field Input", () => {
 
     const newInvoice: InvoiceModel = new InvoiceModel();
     newInvoice.parse("");
-    newInvoice.createUBL();
-    const actual = fs.readFileSync('./invoice1.xml');
+
+    const actual = newInvoice.createUBL();
     const expected = fs.readFileSync('./testOptionalInput/InvoiceLine1O.xml');
-    expect(expected).toStrictEqual(actual);
+    const actualObj = convert(actual, { format: "object" });
+    const expectedObj = convert(expected, { format: "object" });
+    expect(expectedObj).toStrictEqual(actualObj);
   });
   test("Multiple Invoice Line Mandatory Fields", () => {
     const newInvoice: InvoiceModel = new InvoiceModel();
     newInvoice.parse("");
 
-    newInvoice.createUBL();
-    const actual = fs.readFileSync('./invoice1.xml');
+    const actual = newInvoice.createUBL();
     const expected = fs.readFileSync('./testMandatoryInput/InvoiceLine2M.xml');
-    expect(expected).toStrictEqual(actual);
+    const actualObj = convert(actual, { format: "object" });
+    const expectedObj = convert(expected, { format: "object" });
+    expect(expectedObj).toStrictEqual(actualObj);
 
   });
   test("Multiple Invoice Line Optional Fields", () => {
 
     const newInvoice: InvoiceModel = new InvoiceModel();
     newInvoice.parse("");
-    newInvoice.createUBL();
-    const actual = fs.readFileSync('./invoice1.xml');
+
+    const actual = newInvoice.createUBL();
     const expected = fs.readFileSync('./testOptionalInput/InvoiceLine2O.xml');
-    expect(expected).toStrictEqual(actual);
+    const actualObj = convert(actual, { format: "object" });
+    const expectedObj = convert(expected, { format: "object" });
+    expect(expectedObj).toStrictEqual(actualObj);
   });
 });
--- parse.test.ts
+++ parse.test.ts
@@ -226,72 +226,82 @@ import { InvoiceSpecification } from './invoice.json';
 //     }
 //   ],
 // };
+const outputM: InvoiceSpecification = {
+  InvoiceTypeCode: 44,
+  DocumentCurrencyCode: 'AUD',
+  AccountingSupplierParty: { Party: {
+    EndpointID: 2541512,
+    PostalAddress: {
+      Country: {
+        IdentificationCode: 'AU',
+      },
+    },
+    PartyLegalEntity: {
+      RegistrationName: 'Mr HD Man',
+    },
+  }
+},
+  AccountingCustomerParty: { Party: {
+    EndpointID: 14125112,
+    PostalAddress: {
+      Country: {
+        IdentificationCode: 'AU',
+      },
+    },
+    PartyLegalEntity: {
+      RegistrationName: 'Mrs DN Haver',
+    },
+  }
+},
+TaxTotal: [
+    {
+      TaxAmount: 100.05,
+    },
+  ], // An array of 'TaxTotal' is possible for up to 2 'TaxTotal' elements
+  LegalMonetaryTotal: {
+    LineExtensionAmount: 500.68,
+    TaxExclusiveAmount: 500.68,
+    TaxInclusiveAmount: 500.68,
+    PayableAmount: 500.68,
+  },
+  InvoiceLine: [
+    {
+      ID: 124124,
+      InvoicedQuantity: 5,
+      LineExtensionAmount: 1000.05,
+      Item: {
+        Name: 'HD Bestower',
+        ClassifiedTaxCategory: {
+          ID: 'Z',
+          TaxScheme: { ID: 'VAT' },
+        },
+      },
+      Price: {
+        PriceAmount: 1000.05,
+      },
+    },
+  ],
+};
 
 describe('test InvoiceModel parse', () => {
   // Test invoice specification info is correct; return error if not
   test('Mandatory Field Input', () => {
-    const invoiceDataInput = {};
-    // ONLY MANDATORY FIELDS
-    const invoiceDataOutput: InvoiceSpecification = {
-      InvoiceTypeCode: 44,
-      DocumentCurrencyCode: 'AUD',
-      AccountingSupplierParty: { Party: {
-        EndpointID: 2541512,
-        PostalAddress: {
-          Country: {
-            IdentificationCode: 'AU',
-          },
-        },
-        PartyLegalEntity: {
-          RegistrationName: 'Mr HD Man',
-        },
-      }
-    },
-      AccountingCustomerParty: { Party: {
-        EndpointID: 14125112,
-        PostalAddress: {
-          Country: {
-            IdentificationCode: 'AU',
-          },
-        },
-        PartyLegalEntity: {
-          RegistrationName: 'Mrs DN Haver',
-        },
-      }
-    },
-      TaxTotal: [
-        {
-          TaxAmount: 100.05,
-        },
-      ], // An array of 'TaxTotal' is possible for up to 2 'TaxTotal' elements
-      LegalMonetaryTotal: {
-        LineExtensionAmount: 500.68,
-        TaxExclusiveAmount: 500.68,
-        TaxInclusiveAmount: 500.68,
-        PayableAmount: 500.68,
-      },
-      InvoiceLine: [
-        {
-          ID: 124124,
-          InvoicedQuantity: 5,
-          LineExtensionAmount: 1000.05,
-          Item: {
-            Name: 'HD Bestower',
-            ClassifiedTaxCategory: {
-              ID: 'Z',
-              TaxScheme: { ID: 'VAT' },
-            },
-          },
-          Price: {
-            PriceAmount: 1000.05,
-          },
-        },
-      ],
-    };
+    const newInvoice: InvoiceModel = new InvoiceModel();
+    newInvoice.parse("");
+    expect(newInvoice.getInvoiceData()).toStrictEqual(outputM);
 
   });
-  test('Mandatory Fields Missing Input', () => {});
-  test('Mandatory Fields >2 Decimal Place Condition', () => {});
+  test('Mandatory Fields Missing Input', () => {
+    const newInvoice: InvoiceModel = new InvoiceModel();
+    newInvoice.parse("");
+    expect(newInvoice.getInvoiceData()).toThrow(Error);
+
+  });
+  test('Mandatory Fields >2 Decimal Place Condition', () => {
+    const newInvoice: InvoiceModel = new InvoiceModel();
+    newInvoice.parse("");
+    expect(newInvoice.getInvoiceData()).toStrictEqual(outputM);
+  });
   test('Optional Field Input', () => {});
   test('Multiple Invoice Line Mandatory Fields', () => {});
   test('Multiple Invoice Line Optional Fields', () => {});
