e943874412b191bbf9c00682d1c8e0f189227029
Johnny <qinjohnny1@gmail.com>
2023-03-10 18:16:44 +1100

    added parsing input to creatUBL tests

--- createUBL.test.ts
+++ createUBL.test.ts
@@ -1,58 +1,552 @@
 import { InvoiceModel } from "./invoice.model";
 import { InvoiceSpecification } from "./invoice.json";
-const fs = require('fs');
-const { convert } = require('xmlbuilder2');
+const fs = require("fs");
+const { convert } = require("xmlbuilder2");
 
 describe("test InvoiceModel createUBL", () => {
   test("Mandatory Field Input", () => {
     const newInvoice: InvoiceModel = new InvoiceModel();
-    newInvoice.parse(""); // Need to parse some kinda object string from user input
+    newInvoice.parse(`
+    {
+      InvoiceId: "33",
+      IssueDate: "2010-11-09",
+      DueDate: "2011-01-01",
+      InvoiceTypeCode: 44,
+      Note: "Ni Hao Ma?",
+      TaxPointDate: "2012-04-05",
+      InvoiceCurrency: "AUD",
+      TaxCurrency: "AUD",
+      Supplier: {
+        ElectronicAddress: 2541512,
+        PartyName: { Name: "HD Seller" },
+        Address: {
+          addressLine1: "Smith St",
+          addressLine2: "Nothing else",
+          City: "Sydney",
+          PostCode: "The city",
+          countrySubdivision: "Region A",
+          CountryCode: "AU",
+        },
+        PartyLegalEntity: {
+          Name: "Mr HD Man",
+          CompanyID: 1111,
+        },
+        Contact: {
+          Name: "Your Mother",
+          Telephone: 61411111111,
+          ElectronicMail: "givesyouHD@gmail.com",
+        },
+      },
+      Buyer: {
+        electronicAddress: 14125112,
+        PartyName: { Name: "HD Buyer" },
+        Address: {
+          addressLine1: "SomewhereElse Ave",
+          addressLine2: "Nothing else",
+          city: "Sydney",
+          postCode: "The city",
+          countrySubdivision: "Region A",
+          CountryCode: "AU",
+        },
+        PartyLegalEntity: {
+          Name: "Mrs DN Haver",
+          CompanyID: 14124,
+        },
+        Contact: {
+          Name: "Your Father",
+          Telephone: 61422222222,
+          ElectronicMail: "IwantHD@gmail.com",
+        },
+      },
+      TaxTotal: [
+        {
+          TaxAmount: 100.05,
+          TaxSubtotal: {
+            TaxableAmount: 100.05,
+            TaxAmount: 100.05,
+            TaxCategory: {
+              ID: "S",
+              Percent: 100,
+              TaxExemptionReason: "Because i said so",
+              TaxScheme: { ID: "VAT" },
+            },
+          },
+        },
+      ],
+      LegalMonetaryTotal: {
+        LineExtensionAmount: 500.68,
+        TaxExclusiveAmount: 500.68,
+        TaxInclusiveAmount: 500.68,
+        AllowanceTotalAmount: 500.68,
+        ChargeTotalAmount: 500.68,
+        PrepaidAmount: 500.68,
+        PayableRoundingAmount: 500.68,
+        PayableAmount: 500.68,
+      },
+      InvoiceLine: [
+        {
+          ID: 124124,
+          Note: "We take those",
+          InvoicedQuantity: 5,
+          LineExtensionAmount: 1000.05,
+          Item: {
+            Description: "An HD mark",
+            Name: "HD Bestower",
+            VAT: {
+              ID: "S",
+              Percent: 100,
+              TaxScheme: { ID: "VAT" },
+            },
+          },
+          Price: {
+            PriceAmount: 1000.05,
+            BaseQuantity: 5,
+          },
+        },
+      ],
+      }
+    `);
 
     const actual = newInvoice.createUBL();
-    const expected = fs.readFileSync('./testMandatoryInput/InvoiceLine1M.xml');
+    const expected = fs.readFileSync("./testMandatoryInput/InvoiceLine1M.xml");
     const actualObj = convert(actual, { format: "object" });
     const expectedObj = convert(expected, { format: "object" });
     expect(expectedObj).toStrictEqual(actualObj);
   });
   test("Mandatory Fields >2 Decimal Place Condition", () => {
     const newInvoice: InvoiceModel = new InvoiceModel();
-    newInvoice.parse("");
+    newInvoice.parse(`
+    {
+      InvoiceId: "33",
+      IssueDate: "2010-11-09",
+      DueDate: "2011-01-01",
+      InvoiceTypeCode: 44,
+      Note: "Ni Hao Ma?",
+      TaxPointDate: "2012-04-05",
+      InvoiceCurrency: "AUD",
+      TaxCurrency: "AUD",
+      Supplier: {
+        ElectronicAddress: 2541512,
+        PartyName: { Name: "HD Seller" },
+        Address: {
+          addressLine1: "Smith St",
+          addressLine2: "Nothing else",
+          City: "Sydney",
+          PostCode: "The city",
+          countrySubdivision: "Region A",
+          CountryCode: "AU",
+        },
+        PartyLegalEntity: {
+          Name: "Mr HD Man",
+          CompanyID: 1111,
+        },
+        Contact: {
+          Name: "Your Mother",
+          Telephone: 61411111111,
+          ElectronicMail: "givesyouHD@gmail.com",
+        },
+      },
+      Buyer: {
+        electronicAddress: 14125112,
+        PartyName: { Name: "HD Buyer" },
+        Address: {
+          addressLine1: "SomewhereElse Ave",
+          addressLine2: "Nothing else",
+          city: "Sydney",
+          postCode: "The city",
+          countrySubdivision: "Region A",
+          CountryCode: "AU",
+        },
+        PartyLegalEntity: {
+          Name: "Mrs DN Haver",
+          CompanyID: 14124,
+        },
+        Contact: {
+          Name: "Your Father",
+          Telephone: 61422222222,
+          ElectronicMail: "IwantHD@gmail.com",
+        },
+      },
+      TaxTotal: [
+        {
+          TaxAmount: 100.05,
+          TaxSubtotal: {
+            TaxableAmount: 100.05,
+            TaxAmount: 100.05,
+            TaxCategory: {
+              ID: "S",
+              Percent: 100,
+              TaxExemptionReason: "Because i said so",
+              TaxScheme: { ID: "VAT" },
+            },
+          },
+        },
+      ],
+      LegalMonetaryTotal: {
+        LineExtensionAmount: 500.68123,
+        TaxExclusiveAmount: 500.68123,
+        TaxInclusiveAmount: 500.68123,
+        AllowanceTotalAmount: 500.68123,
+        ChargeTotalAmount: 500.68123,
+        PrepaidAmount: 500.68123,
+        PayableRoundingAmount: 500.68123,
+        PayableAmount: 500.68123,
+      },
+      InvoiceLine: [
+        {
+          ID: 124124,
+          Note: "We take those",
+          InvoicedQuantity: 5,
+          LineExtensionAmount: 1000.05,
+          Item: {
+            Description: "An HD mark",
+            Name: "HD Bestower",
+            VAT: {
+              ID: "S",
+              Percent: 100,
+              TaxScheme: { ID: "VAT" },
+            },
+          },
+          Price: {
+            PriceAmount: 1000.05,
+            BaseQuantity: 5,
+          },
+        },
+      ],
+      }
+    `);
 
-    const actual =  newInvoice.createUBL();
-    const expected = fs.readFileSync('./testMandatoryInput/DecimalPointOverM.xml');
+    const actual = newInvoice.createUBL();
+    const expected = fs.readFileSync(
+      "./testMandatoryInput/DecimalPointOverM.xml"
+    );
     const actualObj = convert(actual, { format: "object" });
     const expectedObj = convert(expected, { format: "object" });
     expect(expectedObj).toStrictEqual(actualObj);
   });
   test("Optional Field Input", () => {
-
     const newInvoice: InvoiceModel = new InvoiceModel();
-    newInvoice.parse("");
+    newInvoice.parse(`
+    {
+      InvoiceId: "33",
+      IssueDate: "2010-11-09",
+      DueDate: "2011-01-01",
+      InvoiceTypeCode: 44,
+      Note: "Ni Hao Ma?",
+      TaxPointDate: "2012-04-05",
+      InvoiceCurrency: "AUD",
+      TaxCurrency: "AUD",
+      Supplier: {
+        ElectronicAddress: 2541512,
+        PartyName: { Name: "HD Seller" },
+        Address: {
+          addressLine1: "Smith St",
+          addressLine2: "Nothing else",
+          City: "Sydney",
+          PostCode: "The city",
+          countrySubdivision: "Region A",
+          CountryCode: "AU",
+        },
+        PartyLegalEntity: {
+          Name: "Mr HD Man",
+          CompanyID: 1111,
+        },
+        Contact: {
+          Name: "Your Mother",
+          Telephone: 61411111111,
+          ElectronicMail: "givesyouHD@gmail.com",
+        },
+      },
+      Buyer: {
+        electronicAddress: 14125112,
+        PartyName: { Name: "HD Buyer" },
+        Address: {
+          addressLine1: "SomewhereElse Ave",
+          addressLine2: "Nothing else",
+          city: "Sydney",
+          postCode: "The city",
+          countrySubdivision: "Region A",
+          CountryCode: "AU",
+        },
+        PartyLegalEntity: {
+          Name: "Mrs DN Haver",
+          CompanyID: 14124,
+        },
+        Contact: {
+          Name: "Your Father",
+          Telephone: 61422222222,
+          ElectronicMail: "IwantHD@gmail.com",
+        },
+      },
+      TaxTotal: [
+        {
+          TaxAmount: 100.05,
+          TaxSubtotal: {
+            TaxableAmount: 100.05,
+            TaxAmount: 100.05,
+            TaxCategory: {
+              ID: "S",
+              Percent: 100,
+              TaxExemptionReason: "Because i said so",
+              TaxScheme: { ID: "VAT" },
+            },
+          },
+        },
+      ],
+      LegalMonetaryTotal: {
+        LineExtensionAmount: 500.68,
+        TaxExclusiveAmount: 500.68,
+        TaxInclusiveAmount: 500.68,
+        AllowanceTotalAmount: 500.68,
+        ChargeTotalAmount: 500.68,
+        PrepaidAmount: 500.68,
+        PayableRoundingAmount: 500.68,
+        PayableAmount: 500.68,
+      },
+      InvoiceLine: [
+        {
+          ID: 124124,
+          Note: "We take those",
+          InvoicedQuantity: 5,
+          LineExtensionAmount: 1000.05,
+          Item: {
+            Description: "An HD mark",
+            Name: "HD Bestower",
+            VAT: {
+              ID: "S",
+              Percent: 100,
+              TaxScheme: { ID: "VAT" },
+            },
+          },
+          Price: {
+            PriceAmount: 1000.05,
+            BaseQuantity: 5,
+          },
+        },
+      ],
+      }
+    `);
 
     const actual = newInvoice.createUBL();
-    const expected = fs.readFileSync('./testOptionalInput/InvoiceLine1O.xml');
+    const expected = fs.readFileSync("./testOptionalInput/InvoiceLine1O.xml");
     const actualObj = convert(actual, { format: "object" });
     const expectedObj = convert(expected, { format: "object" });
     expect(expectedObj).toStrictEqual(actualObj);
   });
   test("Multiple Invoice Line Mandatory Fields", () => {
     const newInvoice: InvoiceModel = new InvoiceModel();
-    newInvoice.parse("");
+    newInvoice.parse(`
+    {
+      InvoiceId: "33",
+      IssueDate: "2010-11-09",
+      DueDate: "2011-01-01",
+      InvoiceTypeCode: 44,
+      Note: "Ni Hao Ma?",
+      TaxPointDate: "2012-04-05",
+      InvoiceCurrency: "AUD",
+      TaxCurrency: "AUD",
+      Supplier: {
+        ElectronicAddress: 2541512,
+        PartyName: { Name: "HD Seller" },
+        Address: {
+          addressLine1: "Smith St",
+          addressLine2: "Nothing else",
+          City: "Sydney",
+          PostCode: "The city",
+          countrySubdivision: "Region A",
+          CountryCode: "AU",
+        },
+        PartyLegalEntity: {
+          Name: "Mr HD Man",
+          CompanyID: 1111,
+        },
+        Contact: {
+          Name: "Your Mother",
+          Telephone: 61411111111,
+          ElectronicMail: "givesyouHD@gmail.com",
+        },
+      },
+      Buyer: {
+        electronicAddress: 14125112,
+        PartyName: { Name: "HD Buyer" },
+        Address: {
+          addressLine1: "SomewhereElse Ave",
+          addressLine2: "Nothing else",
+          city: "Sydney",
+          postCode: "The city",
+          countrySubdivision: "Region A",
+          CountryCode: "AU",
+        },
+        PartyLegalEntity: {
+          Name: "Mrs DN Haver",
+          CompanyID: 14124,
+        },
+        Contact: {
+          Name: "Your Father",
+          Telephone: 61422222222,
+          ElectronicMail: "IwantHD@gmail.com",
+        },
+      },
+      TaxTotal: [
+        {
+          TaxAmount: 100.05,
+          TaxSubtotal: {
+            TaxableAmount: 100.05,
+            TaxAmount: 100.05,
+            TaxCategory: {
+              ID: "S",
+              Percent: 100,
+              TaxExemptionReason: "Because i said so",
+              TaxScheme: { ID: "VAT" },
+            },
+          },
+        },
+      ],
+      LegalMonetaryTotal: {
+        LineExtensionAmount: 500.68,
+        TaxExclusiveAmount: 500.68,
+        TaxInclusiveAmount: 500.68,
+        AllowanceTotalAmount: 500.68,
+        ChargeTotalAmount: 500.68,
+        PrepaidAmount: 500.68,
+        PayableRoundingAmount: 500.68,
+        PayableAmount: 500.68,
+      },
+      InvoiceLine: [
+        {
+          ID: 124124,
+          Note: "We take those",
+          InvoicedQuantity: 5,
+          LineExtensionAmount: 1000.05,
+          Item: {
+            Description: "An HD mark",
+            Name: "HD Bestower",
+            VAT: {
+              ID: "S",
+              Percent: 100,
+              TaxScheme: { ID: "VAT" },
+            },
+          },
+          Price: {
+            PriceAmount: 1000.05,
+            BaseQuantity: 5,
+          },
+        },
+      ],
+      }
+    `);
 
     const actual = newInvoice.createUBL();
-    const expected = fs.readFileSync('./testMandatoryInput/InvoiceLine2M.xml');
+    const expected = fs.readFileSync("./testMandatoryInput/InvoiceLine2M.xml");
     const actualObj = convert(actual, { format: "object" });
     const expectedObj = convert(expected, { format: "object" });
     expect(expectedObj).toStrictEqual(actualObj);
-
   });
   test("Multiple Invoice Line Optional Fields", () => {
-
     const newInvoice: InvoiceModel = new InvoiceModel();
-    newInvoice.parse("");
+    newInvoice.parse(`
+    {
+      InvoiceId: "33",
+      IssueDate: "2010-11-09",
+      DueDate: "2011-01-01",
+      InvoiceTypeCode: 44,
+      Note: "Ni Hao Ma?",
+      TaxPointDate: "2012-04-05",
+      InvoiceCurrency: "AUD",
+      TaxCurrency: "AUD",
+      Supplier: {
+        ElectronicAddress: 2541512,
+        PartyName: { Name: "HD Seller" },
+        Address: {
+          addressLine1: "Smith St",
+          addressLine2: "Nothing else",
+          City: "Sydney",
+          PostCode: "The city",
+          countrySubdivision: "Region A",
+          CountryCode: "AU",
+        },
+        PartyLegalEntity: {
+          Name: "Mr HD Man",
+          CompanyID: 1111,
+        },
+        Contact: {
+          Name: "Your Mother",
+          Telephone: 61411111111,
+          ElectronicMail: "givesyouHD@gmail.com",
+        },
+      },
+      Buyer: {
+        electronicAddress: 14125112,
+        PartyName: { Name: "HD Buyer" },
+        Address: {
+          addressLine1: "SomewhereElse Ave",
+          addressLine2: "Nothing else",
+          city: "Sydney",
+          postCode: "The city",
+          countrySubdivision: "Region A",
+          CountryCode: "AU",
+        },
+        PartyLegalEntity: {
+          Name: "Mrs DN Haver",
+          CompanyID: 14124,
+        },
+        Contact: {
+          Name: "Your Father",
+          Telephone: 61422222222,
+          ElectronicMail: "IwantHD@gmail.com",
+        },
+      },
+      TaxTotal: [
+        {
+          TaxAmount: 100.05,
+          TaxSubtotal: {
+            TaxableAmount: 100.05,
+            TaxAmount: 100.05,
+            TaxCategory: {
+              ID: "S",
+              Percent: 100,
+              TaxExemptionReason: "Because i said so",
+              TaxScheme: { ID: "VAT" },
+            },
+          },
+        },
+      ],
+      LegalMonetaryTotal: {
+        LineExtensionAmount: 500.68,
+        TaxExclusiveAmount: 500.68,
+        TaxInclusiveAmount: 500.68,
+        AllowanceTotalAmount: 500.68,
+        ChargeTotalAmount: 500.68,
+        PrepaidAmount: 500.68,
+        PayableRoundingAmount: 500.68,
+        PayableAmount: 500.68,
+      },
+      InvoiceLine: [
+        {
+          ID: 124124,
+          Note: "We take those",
+          InvoicedQuantity: 5,
+          LineExtensionAmount: 1000.05,
+          Item: {
+            Description: "An HD mark",
+            Name: "HD Bestower",
+            VAT: {
+              ID: "S",
+              Percent: 100,
+              TaxScheme: { ID: "VAT" },
+            },
+          },
+          Price: {
+            PriceAmount: 1000.05,
+            BaseQuantity: 5,
+          },
+        },
+      ],
+      }
+    `);
 
     const actual = newInvoice.createUBL();
-    const expected = fs.readFileSync('./testOptionalInput/InvoiceLine2O.xml');
+    const expected = fs.readFileSync("./testOptionalInput/InvoiceLine2O.xml");
     const actualObj = convert(actual, { format: "object" });
     const expectedObj = convert(expected, { format: "object" });
     expect(expectedObj).toStrictEqual(actualObj);
--- parse.test.ts
+++ parse.test.ts
@@ -88,7 +88,7 @@ import { InvoiceSpecification } from './invoice.json';
 //     AllowanceTotalAmount?: 500.68,
 //     ChargeTotalAmount?: 500.68,
 //     PrepaidAmount?: 500.68,
-//     PayableRoundingAmount: 500.68,
+//     PayableRoundingAmount?: 500.68,
 //     PayableAmount: 500.68,
 //   },
 //   InvoiceLine: [
@@ -115,117 +115,105 @@ import { InvoiceSpecification } from './invoice.json';
 // };
 
 // Planned input (subject to change)
-// const invoiceData: InvoiceSpecification = {
-//   CustomizationID?: "urn:cen.eu:en16931:2017#compliant#urn:fdc:peppol.eu:2017:poacc:billing:3.0",
-//   ProfileID?: "urn:fdc:peppol.eu:2017:poacc:billing:01:1.0",
-//   ID?: "33",
-//   IssueDate?: 2010 - 11 - 09,
-//   DueDate?: 2011 - 01 - 01,
-//   InvoiceTypeCode: 44,
-//   Note?: 'Ni Hao Ma?',
-//   TaxPointDate?: 2012 - 04 - 05,
-//   DocumentCurrencyCode: "AUD",
-//   TaxCurrencyCode?: "AUD",
-//   AccountingSupplierParty: { Party:
-//    {
-//     EndpointID: 2541512,
-//     PartyName?: { Name: "HD Seller" },
-//     PostalAddress: {
-//       StreetName?: "Smith St",
-//       AdditionalStreetName?: "Nothing else",
-//       CityName?: "Sydney",
-//       PostalZone?: "The city",
-//       CountrySubentity?: "Region A",
-//       Country: {
-//         IdentificationCode: "AU",
-//       }
-//     },
-//     PartyLegalEntity: {
-//       RegistrationName: "Mr HD Man",
-//       CompanyID?: 1111,
-//     },
-//     Contact?: {
-//       Name?: "Your Mother",
-//       Telephone?: 0411111111,
-//       ElectronicMail?: "givesyouHD@gmail.com",
-//     }
-//     }
-//   },
-//   AccountingCustomerParty: { Party:
-//    {
-//     EndpointID: 14125112,
-//     PartyName?: { Name: "HD Buyer" },
-//     PostalAddress: {
-//       StreetName?: "SomewhereElse Ave",
-//       AdditionalStreetName?: "Nothing else",
-//       CityName?: "Sydney",
-//       PostalZone?: "The city",
-//       CountrySubentity?: "Region A",
-//       Country: {
-//         IdentificationCode: "AU",
-//       }
-//     },
-//     PartyLegalEntity: {
-//       RegistrationName: "Mrs DN Haver",
-//       CompanyID?: 14124,
-//     },
-//     Contact?: {
-//       Name?: "Your Father",
-//       Telephone?: 0422222222,
-//       ElectronicMail?: "IwantHD@gmail.com",
-//     }
-//     }
-//   },
-//   TaxTotal: [
-//     {
-//       TaxAmount: 100.05,
-//       TaxSubtotal?: {
-//         TaxableAmount: 100.05,
-//         TaxAmount: 100.05,
-//         TaxCategory: {
-//           ID: "E",
-//           Percent?: 100,
-//           TaxExemptionReason?: "Because i said so",
-//           TaxScheme: { ID: "VAT" },
-//         }
-//       }
-//     },
-//     {
-//       TaxAmount: 100.05,
-//     }
-//   ], // An array of 'TaxTotal' is possible for up to 2 'TaxTotal' elements
-//   LegalMonetaryTotal: {
-//     LineExtensionAmount: 500.68,
-//     TaxExclusiveAmount: 500.68,
-//     TaxInclusiveAmount: 500.68,
-//     AllowanceTotalAmount?: 500.68,
-//     ChargeTotalAmount?: 500.68,
-//     PrepaidAmount?: 500.68,
-//     PayableRoundingAmount: 500.68,
-//     PayableAmount: 500.68,
-//   },
-//   InvoiceLine: [
-//     {
-//       ID: 124124,
-//       Note?: "We take those",
-//       InvoicedQuantity: 5,
-//       LineExtensionAmount: 1000.05,
-//       Item: {
-//         Description?: "An HD mark",
-//         Name: "HD Bestower",
-//         ClassifiedTaxCategory: {
-//           ID: "E",
-//           Percent?: 100,
-//           TaxScheme: { ID: "VAT" },
-//         }
-//       },
-//       Price: {
-//         PriceAmount: 1000.05,
-//         BaseQuantity?: 5,
-//       }
-//     }
-//   ],
-// };
+type invoiceData = {
+  InvoiceId?: string;
+  IssueDate?: string;
+  DueDate?: string;
+  InvoiceTypeCode: number;
+  Note?: string;
+  TaxPointDate?: string;
+  InvoiceCurrency: string;
+  TaxCurrency?: string;
+  Supplier: {
+    ElectronicAddress: string;
+    PartyName?: { Name: string };
+    Address: {
+      addressLine1?: string;
+      addressLine2?: string;
+      City?: string;
+      PostCode?: string;
+      countrySubdivision?: string;
+      CountryCode: string;
+    };
+    PartyLegalEntity: {
+      Name: string;
+      CompanyID?: number;
+    };
+    Contact?: {
+      Name?: string;
+      Telephone?: number;
+      ElectronicMail?: string;
+    };
+  };
+  Buyer: {
+    electronicAddress: number;
+    PartyName?: { Name: string };
+    Address: {
+      addressLine1?: string;
+      addressLine2?: string;
+      city?: string;
+      postCode?: string;
+      countrySubdivision?: string;
+      CountryCode: string;
+    };
+    PartyLegalEntity: {
+      Name: string;
+      CompanyID?: number;
+    };
+    Contact?: {
+      Name?: string;
+      Telephone?: number;
+      ElectronicMail?: string;
+    };
+  };
+  TaxTotal: [
+    {
+      TaxAmount: number;
+      TaxSubtotal?: {
+        TaxableAmount: number;
+        TaxAmount: number;
+        TaxCategory: {
+          ID: string;
+          Percent?: number;
+          TaxExemptionReason?: string;
+          TaxScheme: { ID: string };
+        };
+      };
+    },
+  ];
+  LegalMonetaryTotal: {
+    LineExtensionAmount: number;
+    TaxExclusiveAmount: number;
+    TaxInclusiveAmount: number;
+    AllowanceTotalAmount?: number;
+    ChargeTotalAmount?: number;
+    PrepaidAmount?: number;
+    PayableRoundingAmount?: number;
+    PayableAmount: number;
+  };
+  InvoiceLine: [
+    {
+      ID: number;
+      Note?: string;
+      InvoicedQuantity: number;
+      LineExtensionAmount: number;
+      Item: {
+        Description?: string;
+        Name: string;
+        VAT: {
+          ID: string;
+          Percent?: number;
+          TaxScheme: { ID: string };
+        };
+      };
+      Price: {
+        PriceAmount: number;
+        BaseQuantity?: number;
+      };
+    },
+  ];
+};
+
 const outputM: InvoiceSpecification = {
   InvoiceTypeCode: 44,
   DocumentCurrencyCode: 'AUD',
