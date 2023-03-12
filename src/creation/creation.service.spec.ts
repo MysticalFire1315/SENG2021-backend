3d554301937275ca25b4affc1d01e3d333e43a12
MysticalFire1315 <joelchong03@gmail.com>
2023-03-12 17:16:25 +1100

    Added controller and service tests

--- src/creation/creation.service.spec.ts
+++ src/creation/creation.service.spec.ts
@@ -1,10 +1,18 @@
-import { StreamableFile } from '@nestjs/common';
 import { Test, TestingModule } from '@nestjs/testing';
+import { readFileSync } from 'fs';
+import { join } from 'path';
 import { Readable } from 'stream';
+import { convert } from 'xmlbuilder2';
 import { CreationService } from './creation.service';
+import { testAssetsPath } from './model/spec.config';
+
+const path = join(process.cwd(), testAssetsPath);
 
 describe('CreationService', () => {
   let service: CreationService;
+  const readFile: (filePath: string) => string = (filePath) =>
+    readFileSync(filePath).toString();
+
   const toFile: (contents: string) => Express.Multer.File = (contents) => {
     const bufferContents = Buffer.from(contents);
     const testFile: Express.Multer.File = {
@@ -36,29 +44,41 @@ describe('CreationService', () => {
   });
 
   describe('upload', () => {
-    it('test stub', () => {
+    it('Upload success', async () => {
       expect(() => {
-        service.invoiceUpload(toFile('{"a": "hello"}')).then((value) => {
-          expect(value).toStrictEqual({ timeEstimate: 1, token: 'abc' });
-        });
+        service
+          .invoiceUpload(
+            toFile(readFile(path + 'inputs/compulsory/InvoiceLine1M.json')),
+          )
+          .then((value) => {
+            expect(value).toStrictEqual({ timeEstimate: 1, token: 'abc' });
+          });
       }).not.toThrowError();
     });
   });
 
   describe('download', () => {
-    it('test stub', () => {
+    it('Download success', () => {
       expect(() => {
-        service.invoiceUpload(toFile('{"a": "hello"}')).then((value) => {
-          expect(value).toStrictEqual({ timeEstimate: 1, token: 'abc' });
-        });
+        service
+          .invoiceUpload(
+            toFile(readFile(path + 'inputs/compulsory/InvoiceLine1M.json')),
+          )
+          .then((value) => {
+            expect(value).toStrictEqual({ timeEstimate: 1, token: 'abc' });
+          });
       }).not.toThrowError();
 
       expect(() => {
         service.invoiceDownload('abc').then((value) => {
-          const expectedOutput = new StreamableFile(
-            Buffer.from('sample output'),
+          const expectedOutput = readFile(
+            path + 'outputs/compulsory/InvoiceLine1M.xml',
           );
-          expect(value.getStream()).toStrictEqual(expectedOutput.getStream());
+          const actualOutput = value.getStream().read().toString();
+
+          const actualObj = convert(actualOutput, { format: 'object' });
+          const expectedObj = convert(expectedOutput, { format: 'object' });
+          expect(actualObj).toStrictEqual(expectedObj);
         });
       }).not.toThrowError();
     });
