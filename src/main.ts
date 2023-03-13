0400e6ca808e7c626a5754091f829fa28c54457c
Johnny <qinjohnny1@gmail.com>
2023-03-13 12:22:58 +1100

    swagger stuff

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
     logger: ['debug'],
   });
+  const config = new DocumentBuilder()
+    .setTitle('API E-invoice Creator')
+    .setDescription('Creates an E-invoice in UBL XML Format')
+    .setVersion('1.0')
+    .addTag('E-invoice Creation')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
