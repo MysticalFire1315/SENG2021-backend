d16dcdd83840118136cbb4d50c17477043122ac5
MysticalFire1315 <joelchong03@gmail.com>
2023-03-23 13:07:57 +1100

    Updated descriptions for response

--- src/creation/entity/file-upload-response.entity.ts
+++ src/creation/entity/file-upload-response.entity.ts
@@ -1,4 +1,12 @@
 export class FileUploadResponseEntity {
+  /**
+   * A time estimate for how long it will take to parse the file.
+   */
   timeEstimate: number;
+
+  /**
+   * A token associated with the uploaded file. This is to be used to retrieve
+   * the UBL version of the file.
+   */
   token: string;
 }
