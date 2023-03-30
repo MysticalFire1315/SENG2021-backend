f1909fb9925493c7bf2dc684634af7d9a196f96a
MysticalFire1315 <joelchong03@gmail.com>
2023-03-30 15:17:33 +1100

    Added exception filter so responses don't attempt to attach file

--- src/creation/creation.module.ts
+++ src/creation/creation.module.ts
@@ -1,9 +1,17 @@
 import { Module } from '@nestjs/common';
+import { APP_FILTER } from '@nestjs/core';
+import { HttpExceptionFilter } from 'src/http-exception.filter';
 import { CreationController } from './creation.controller';
 import { CreationService } from './creation.service';
 
 @Module({
   controllers: [CreationController],
-  providers: [CreationService],
+  providers: [
+    CreationService,
+    {
+      provide: APP_FILTER,
+      useClass: HttpExceptionFilter,
+    },
+  ],
 })
 export class CreationModule {}
