72f563761a47cb026e16698f4a308e9f1be1d3e7
MysticalFire1315 <joelchong03@gmail.com>
2023-04-02 18:44:52 +1000

    Drafted route for frontend

--- src/frontend/frontend.controller.ts
+++ src/frontend/frontend.controller.ts
@@ -1,4 +1,18 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { ApiTags } from '@nestjs/swagger';
+import { FrontendService } from './frontend.service';
+import { CreateInvoiceDto } from './dto/create-invoice.dto';
 
+@ApiTags('frontend')
 @Controller('frontend')
-export class FrontendController {}
+export class FrontendController {
+  constructor(private frontendService: FrontendService) {}
+
+  @Post()
+  async createInvoice(
+    @Body() createInvoiceDto: CreateInvoiceDto,
+  ): Promise<String> {
+    const output = await this.frontendService.createInvoice(createInvoiceDto);
+    return output;
+  }
+}
--- src/frontend/frontend.service.ts
+++ src/frontend/frontend.service.ts
@@ -1,4 +1,23 @@
 import { Injectable } from '@nestjs/common';
+import { CreateInvoiceDto } from './dto/create-invoice.dto';
+import { CreationApi } from './api/creation.api';
+import { ValidationApi } from './api/validation.api';
 
 @Injectable()
-export class FrontendService {}
+export class FrontendService {
+  private readonly creationApi = new CreationApi();
+  private readonly validationApi = new ValidationApi();
+
+  async createInvoice(createInvoiceDto: CreateInvoiceDto): Promise<String> {
+    // Parse Dto
+    const invoiceParsed = createInvoiceDto.toString();
+
+    // Call creation api
+    const invoiceString = await this.creationApi.request(invoiceParsed, 'json');
+
+    // Call validation api
+    const invoiceValid = await this.validationApi.request(invoiceString);
+
+    return invoiceString;
+  }
+}
