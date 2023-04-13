473d5ea9794c8073230f796850972b2ce3ee93f7
MysticalFire1315 <joelchong03@gmail.com>
2023-04-14 01:15:54 +1000

    Updated rendering to v3

--- src/frontend/api/rendering.api.ts
+++ src/frontend/api/rendering.api.ts
@@ -4,7 +4,7 @@ import { ApiError } from './error.api';
 
 export class RenderingApi {
   private axiosInstance = axios.create({
-    baseURL: 'https://macroservices.masterofcubesau.com/api/v2/',
+    baseURL: 'https://macroservices.masterofcubesau.com/api/v3/',
   });
 
   private key: string = undefined;
